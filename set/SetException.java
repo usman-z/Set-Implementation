c9c85448ac7f079b34d4b5d4103b5695db35905f
Callie <119629730+calliemh@users.noreply.github.com>
2022-12-04 15:48:44 -0500

    Update SetException.java

--- set/SetException.java
+++ set/SetException.java
@@ -1,11 +1,19 @@
 package set;
 
+/**
+ *
+ * @author Usman
+ */
 public class SetException extends RuntimeException {
 
     public SetException() {
         super();
     }
 
+    /**
+     *
+     * @param s is the String
+     */
     public SetException(String s) {
         super(s);
     }
