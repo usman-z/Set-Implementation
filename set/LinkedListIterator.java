8faea455caf249ad59d1a0da900e04d37d172e45
Callie <119629730+calliemh@users.noreply.github.com>
2022-12-04 11:33:36 -0500

    Update LinkedListIterator.java

--- set/LinkedListIterator.java
+++ set/LinkedListIterator.java
@@ -6,4 +6,36 @@ package set;
  */
 public class LinkedListIterator {
     
+    Node<T> current;
+
+    /**
+     *
+     * @param newNode is the node
+     */
+    LinkedListIterator(Node<T> newNode) {
+        this.current = newNode;
+    }
+
+    /**
+     *
+     * @return is true if there are more elements
+     */
+    public boolean hasNext() {
+        return current != null;
+    }
+
+    /**
+     *
+     * @return is the next element
+     */
+    public T next() {
+        if (current != null) {
+            T element = current.element;
+            current = current.next;
+            return element;
+        } else {
+            return null;
+        }
+    }
+
 }
